e8f62dc3995134579a5d05e97f0926976aeb907d
MAHMOUD2ABDALLAH <Mahmoud_abdallah20@outlook.com>
2025-02-12 09:02:16 +0200

    Initial commit

---
+++ Main.py
@@ -0,0 +1,8 @@
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+import seaborn as sns
+
+
+list = ['']
