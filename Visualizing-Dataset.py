68350e4e4c4fafbaf81d3ffedfacb70f8a1f3f15
Mahmoud Abdallah <105394210+MAHMOUD2ABDALLAH@users.noreply.github.com>
2025-06-18 11:54:42 +0300

    Reconstrution of new branch about visualizing dataset

--- Visualizing-Dataset.py
+++ Visualizing-Dataset.py
@@ -2,10 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import os
+
+repo_path = os.path.expanduser('Canada.xlsx')
 
 df_Canada = pd.read_excel(
-    'C:\\Users\Mahmoud\Microsoft-PowerUp\Mahmoud Mohamed Abdallah - Documents\introDataAnalyse\Canada.xlsx',
-    skiprows=range(20), skipfooter=2)
+    os.path.join(repo_path),
+    skiprows=range(20),
+    skipfooter=29
+)
 print('Data downloaded and read into a dataframe!')
 
 years = list(map(str, range(1980, 2014)))
@@ -26,61 +31,64 @@ df_all = df_all.transpose()
 print(df_all.head())
 df_all.index = df_all.index.map(int)
 df_all.plot(kind='line')
+plt.s
 plt.title('Immigrants from All over the world')
 plt.ylabel('Number of Immigrants')
 plt.xlabel('Years')
 plt.xlim(1980, 2013)
 plt.ylim(0, 70000)
-plt.legend(fontsize='5', loc='upper right', ncol=8)
+plt.legend(fontsize=5, handlelength=1, labelspacing=0.2, borderpad=1.5,
+           loc='upper right', bbox_to_anchor=(1.24, 1),ncol=8)
+plt.autoscale(enable=True, axis='x', tight=True)
 plt.grid(True)
 plt.show()
-# print("--------Let's compare the number of immigrants of each Asia,Europe and Africa from 1980 to 2013.--------------")
-# # Prompt the user for their choice
-# choice_num = int(input(" 1-Asia\n 2-Europe\n 3-Africa\nEnter the number corresponding to your choice: "))
-#
-# # Check if the choice is valid and handle each region accordingly
-# if choice_num == 1:
-#     condition = df_Canada['Continent']=='Asia'
-#     df_condition = df_Canada.loc[condition, years]
-#     df_condition = df_condition.transpose()
-#     print(df_condition.head())
-#     print("---------------------------------------------------------------------------------------------------")
-#     df_condition.plot(kind='line')
-#     plt.title('Immigrants from Asia')
-#     plt.ylabel('Number of Immigrants')
-#     plt.xlabel('Years')
-#     plt.xlim(0, 33)
-#     plt.ylim(0, 70000)
-#     plt.legend(fontsize='5', loc='upper right', ncol=8)
-#     plt.grid(True)
-#     plt.show()
-# elif choice_num == 2:
-#     condition = df_Canada['Continent'] == 'Europe'
-#     df_condition = df_Canada.loc[condition, years]
-#     df_condition = df_condition.transpose()
-#     print(df_condition.head())
-#     print("---------------------------------------------------------------------------------------------------")
-#     df_condition.plot(kind='line')
-#     plt.title('Immigrants from Europe')
-#     plt.ylabel('Number of Immigrants')
-#     plt.xlabel('Years')
-#     plt.xlim(0, 33)
-#     plt.ylim(0, 70000)
-#     plt.legend(fontsize='5', loc='upper right', ncol=8)
-#     plt.grid(True)
-#     plt.show()
-# elif choice_num == 3:
-#     condition = df_Canada['Continent'] == 'Africa'
-#     df_condition = df_Canada.loc[condition, years]
-#     df_condition = df_condition.transpose()
-#     print(df_condition.head())
-#     print("---------------------------------------------------------------------------------------------------")
-#     df_condition.plot(kind='line')
-#     plt.title('Immigrants from Africa')
-#     plt.ylabel('Number of Immigrants')
-#     plt.xlabel('Years')
-#     plt.xlim(0, 33)
-#     plt.ylim(0, 70000)
-#     plt.legend(fontsize='5', loc='upper right', ncol=8)
-#     plt.grid(True)
-#     plt.show()
+print("--------Let's compare the number of immigrants of each Asia,Europe and Africa from 1980 to 2013.--------------")
+# Prompt the user for their choice
+choice_num = int(input(" 1-Asia\n 2-Europe\n 3-Africa\nEnter the number corresponding to your choice: "))
+
+# Check if the choice is valid and handle each region accordingly
+if choice_num == 1:
+    condition = df_Canada['Continent'] == 'Asia'
+    df_condition = df_Canada.loc[condition, years]
+    df_condition = df_condition.transpose()
+    print(df_condition.head())
+    print("---------------------------------------------------------------------------------------------------")
+    df_condition.plot(kind='line')
+    plt.title('Immigrants from Asia')
+    plt.ylabel('Number of Immigrants')
+    plt.xlabel('Years')
+    plt.xlim(0, 33)
+    plt.ylim(0, 70000)
+    plt.legend(fontsize='5', loc='upper right', ncol=8)
+    plt.grid(True)
+    plt.show()
+elif choice_num == 2:
+    condition = df_Canada['Continent'] == 'Europe'
+    df_condition = df_Canada.loc[condition, years]
+    df_condition = df_condition.transpose()
+    print(df_condition.head())
+    print("---------------------------------------------------------------------------------------------------")
+    df_condition.plot(kind='line')
+    plt.title('Immigrants from Europe')
+    plt.ylabel('Number of Immigrants')
+    plt.xlabel('Years')
+    plt.xlim(0, 33)
+    plt.ylim(0, 70000)
+    plt.legend(fontsize='5', loc='upper right', ncol=8)
+    plt.grid(True)
+    plt.show()
+elif choice_num == 3:
+    condition = df_Canada['Continent'] == 'Africa'
+    df_condition = df_Canada.loc[condition, years]
+    df_condition = df_condition.transpose()
+    print(df_condition.head())
+    print("---------------------------------------------------------------------------------------------------")
+    df_condition.plot(kind='line')
+    plt.title('Immigrants from Africa')
+    plt.ylabel('Number of Immigrants')
+    plt.xlabel('Years')
+    plt.xlim(0, 33)
+    plt.ylim(0, 70000)
+    plt.legend(fontsize='5', loc='upper right', ncol=8)
+    plt.grid(True)
+    plt.show()
