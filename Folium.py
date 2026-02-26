64f699fa98daa082fc25ff68fb2e04fbd8c3510c
Mahmoud Abdallah <105394210+MAHMOUD2ABDALLAH@users.noreply.github.com>
2026-02-26 04:01:14 +0200

    Clean up Folium.py by removing unused code
    Removed unnecessary imports and data processing steps related to the Canada dataset.

--- Folium.py
+++ Folium.py
@@ -1,43 +1,9 @@
-from pkgutil import extend_path
-from string import printable
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import folium
-import os
-
-repo_path = os.path.expanduser("Canada.xlsx")
-
-df_Canada = pd.read_excel(os.path.join(repo_path), skiprows=range(20), skipfooter=29)
-
-print("Data downloaded and read into a dataframe!")
-
-years = list(map(str, range(1980, 2014)))
-
-# drop unnecessary columns
-df_Canada.drop(["AREA", "REG", "DEV", "Type", "Coverage"], axis=1, inplace=True)
-# rename columns
-df_Canada.rename(
-    columns={"OdName": "Country", "AreaName": "Continent", "RegName": "Region"},
-    inplace=True,
-)
-
-# let's examine the types of the column labels
-all(isinstance(column, str) for column in df_Canada.columns)
-
-# it returns False, we need to convert them to strings
-df_Canada.columns = list(map(str, df_Canada.columns))
-
-# set the country name as index - useful for quickly looking up countries using .loc method
-df_Canada.set_index("Country", inplace=True)
-
-# add total column
-df_Canada["Total"] = df_Canada.select_dtypes(include="number").sum(axis=1)
-
-# let's view the first five elements and see how the dataframe was changed
-print(df_Canada.head())
 
 # define the world map
 world_map = folium.Map()
